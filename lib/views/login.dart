8a3a147e0ed64933da3be582d010c06e4d8b8793
JancoSOUS <jancobuitendag@gmail.com>
2021-10-27 13:03:05 +0200

    Made Login Screen Scrollable.

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -19,63 +19,65 @@ class _LoginState extends State<Login> {
         backgroundColor: Colors.lightBlue,
         title: Text("Login"),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 40, bottom: 20),
-            child: Text(
-              "Please Enter your student number and password ",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(hintText: "Student Number"),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(hintText: "Password"),
-              obscureText: true,
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Checkbox(
-                      value: autoLogin,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          autoLogin = value!;
-                        });
-                      }),
-                  Text("Keep me logged in")
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  fixedSize: Size(buttonWidth, buttonHeight),
-                  primary: Colors.lightBlue),
-              onPressed: () {},
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 20),
               child: Text(
-                "Login",
-                style: TextStyle(color: Colors.black),
+                "Please Enter your student number and password ",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(hintText: "Student Number"),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(hintText: "Password"),
+                obscureText: true,
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                        value: autoLogin,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            autoLogin = value!;
+                          });
+                        }),
+                    Text("Keep me logged in")
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(buttonWidth, buttonHeight),
+                    primary: Colors.lightBlue),
+                onPressed: () {},
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
