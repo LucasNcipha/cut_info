d1adcc7e317dc3c2835e948e30cabbfc06680171
JancoSOUS <jancobuitendag@gmail.com>
2021-11-16 15:21:21 +0200

    Fixed Login screen, progress indicator and layout

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -40,110 +40,118 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.blue.shade700, Colors.lightBlue.shade50])),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Image(
-                  image: AssetImage('assets/logo.png'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40, bottom: 20),
-                child: Text(
-                  'Please Enter your email and password ',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ),
-              AppTextField(
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-                labelText: 'Please enter email address',
-              ),
-              AppTextField(
-                hideText: true,
-                keyboardType: TextInputType.text,
-                controller: passwordController,
-                labelText: 'Please enter your password',
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Checkbox(
-                          value: autoLogin,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              autoLogin = value!;
-                            });
-                          }),
-                      Text('Keep me logged in')
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: Size(buttonWidth, buttonHeight),
-                      primary: Colors.lightBlue),
-                  onPressed: () {
-                    loginUserInUI(context,
-                        email: emailController.text.trim(),
-                        password: passwordController.text);
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'Dont have an account yet ? ',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  primary: Colors.lightBlue,
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pushNamed(RouteManager.registerPage);
-                },
-                child: const Text('Sign up'),
-              ),
-              Selector<UserService, Tuple2>(
-                selector: (context, value) =>
-                    Tuple2(value.showUserProgress, value.userProgressText),
-                builder: (context, value, child) {
-                  return value.item1
-                      ? AppProgressIndicator(text: '${value.item2}')
-                      : Container();
-                },
-              ),
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade700, Colors.lightBlue.shade50],
           ),
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Image(
+                        image: AssetImage('assets/logo.png'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40, bottom: 20),
+                      child: Text(
+                        'Please Enter your email and password ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    AppTextField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                      labelText: 'Please enter email address',
+                    ),
+                    AppTextField(
+                      hideText: true,
+                      keyboardType: TextInputType.text,
+                      controller: passwordController,
+                      labelText: 'Please enter your password',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Checkbox(
+                            value: autoLogin,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                autoLogin = value!;
+                              });
+                            }),
+                        Text('Keep me logged in')
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: Size(buttonWidth, buttonHeight),
+                          primary: Colors.lightBlue),
+                      onPressed: () {
+                        loginUserInUI(context,
+                            email: emailController.text.trim(),
+                            password: passwordController.text);
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text(
+                        'Dont have an account yet ? ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        primary: Colors.lightBlue,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed(RouteManager.registerPage);
+                      },
+                      child: const Text('Sign up'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Selector<UserService, Tuple2>(
+              selector: (context, value) =>
+                  Tuple2(value.showUserProgress, value.userProgressText),
+              builder: (context, value, child) {
+                return value.item1
+                    ? AppProgressIndicator(text: '${value.item2}')
+                    : Container();
+              },
+            ),
+          ],
         ),
       ),
     );
