1b6e4040607fee1a1624d023b6fe7157241109da
Erdwurm05 <ruanfoord50@gmail.com>
2021-11-18 18:00:29 +0200

    made main_page class

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class main_page extends StatefulWidget {
+  const main_page({Key? key}) : super(key: key);
+
+  @override
+  _main_pageState createState() => _main_pageState();
+}
+
+class _main_pageState extends State<main_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
