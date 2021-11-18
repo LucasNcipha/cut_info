8f1e5d177ee7fb24b2bd8f8e8830480773fbccb0
Erdwurm05 <ruanfoord50@gmail.com>
2021-11-18 18:15:46 +0200

    changes to post_card class

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -4,35 +4,46 @@ import 'package:flutter/material.dart';
 class post_card extends StatelessWidget {
   const post_card({
     Key? key,
-    required String postTitle,
-    required String postText,
-    required bool containsImage,
+    required this.postTitle,
+    required this.postText,
+    required this.containsImage,
   }) : super(key: key);
 
-  final postTitle = '';
-  final postText = '';
-  final bool containsImage = false;
+  final postTitle;
+  final postText;
+  final bool containsImage;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.black,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 20),
-              child: Text(postTitle,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600)),
-            ),
-            Text(
-              postText,
-              style: TextStyle(color: Colors.white, fontSize: 10),
-            )
-          ],
-        ));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          color: Colors.blue.shade700,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(postTitle,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20, left: 10),
+                    child: Text(
+                      postText,
+                      style: TextStyle(color: Colors.black, fontSize: 10),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )),
+    );
   }
 }
