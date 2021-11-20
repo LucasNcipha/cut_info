70ad7860c160588fa20daa509d0e576cb8ff0a96
JancoSOUS <jancobuitendag@gmail.com>
2021-11-20 23:14:00 +0200

    added fields to postcard

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -3,32 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class PostCard extends StatelessWidget {
-  const PostCard({
-    Key? key,
-    required this.postTitle,
-    required this.postText,
-    required this.containsImage,
-  }) : super(key: key);
+  const PostCard(
+      {Key? key,
+      required this.postTitle,
+      required this.postText,
+      required this.containsImage,
+      required this.dateTime})
+      : super(key: key);
 
   final postTitle;
   final postText;
   final bool containsImage;
+  final DateTime? dateTime;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-
       child: ListTile(
         tileColor: Colors.lightBlue,
         title: Text("$postTitle"),
-        subtitle: Text(
-            "$postText"
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("$postText"),
+            Text("$dateTime"),
+          ],
         ),
-
         isThreeLine: true,
       ),
-
-
     );
   }
 }
