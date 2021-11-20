7cdd2f386fade828d6a2baf6de600a62552a7671
JancoSOUS <jancobuitendag@gmail.com>
2021-11-20 23:21:27 +0200

    Added Post Functionality and Shows Posts from dtb.

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,18 @@
+import 'package:backendless_sdk/backendless_sdk.dart';
 import 'package:cut_info/models/post.dart';
+// ignore: unused_import
 import 'package:cut_info/services/helper_todo.dart';
+// ignore: unused_import
+import 'package:cut_info/services/user_service.dart';
+import 'package:cut_info/widgets/app_progress_indicator.dart';
+// ignore: unused_import
+import 'package:cut_info/widgets/app_textfield.dart';
+import 'package:cut_info/widgets/dialogs.dart';
 import 'package:cut_info/widgets/post_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:tuple/tuple.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -12,27 +22,44 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  late TextEditingController postTitleController;
+  late TextEditingController postContentController;
 
+  final List<Posts> posts = List.empty(growable: true);
 
-
-  late TextEditingController postController;
-  final posts = List<Post>.generate(10, (index) => Post("Tile $index","Content $index",false));
   @override
   void initState() {
     super.initState();
-    postController = TextEditingController();
+    postTitleController = TextEditingController();
+    postContentController = TextEditingController();
+    Backendless.data.of("General").find().then((tablePosts) {
+      tablePosts!.forEach((element) {
+        setState(() {
+          Posts post = new Posts(element?["title"], element?["content"],
+              element?["hasImage"], element?["created"]);
+          posts.add(post);
+        });
+
+        print("title ${posts[0].title}");
+        print("content ${posts[0].content}");
+        print("hasImage ${posts[0].hasImage}");
+        print("created ${posts[0].created}");
+      });
+    });
   }
 
   @override
   void dispose() {
-    postController.dispose();
+    postTitleController.dispose();
+    postContentController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
+    /////////////alertDialog//////////
     AlertDialog alertDialog = AlertDialog(
+      backgroundColor: Colors.blue,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
@@ -40,24 +67,57 @@ class _MainPageState extends State<MainPage> {
       content: Column(
         children: [
           TextField(
-
+            keyboardType: TextInputType.text,
+            controller: postTitleController,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), hintText: 'Enter Post Title.'),
           ),
-       ////////////////////TEXTBOX
-
+          TextField(
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            controller: postContentController,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), hintText: 'Enter Post content.'),
+          ),
         ],
       ),
       actions: [
         TextButton(
-          child: Text('Cancel'),
+          child: Text(
+            'Cancel',
+            style: TextStyle(color: Colors.black),
+          ),
           onPressed: () {
-            Navigator.pop(context);
+            postTitleController.text = '';
+            postContentController.text = '';
+            Navigator.of(context).pop();
           },
         ),
         TextButton(
-          child: Text('Save'),
+          child: Text(
+            'Save',
+            style: TextStyle(color: Colors.black),
+          ),
           onPressed: () async {
-            createNewTodoInUI(context,
-                titleController: postController);
+            Posts newPost = new Posts(postTitleController.text,
+                postContentController.text, false, DateTime.now());
+
+            Map data = {
+              'title': newPost.title,
+              'content': newPost.content,
+              'hasImage': newPost.hasImage,
+              'created': newPost.created
+            };
+
+            Backendless.data.of("General").save(data).then((value) {
+              postTitleController.text = '';
+              postContentController.text = '';
+              Navigator.of(context).pop();
+
+              AppProgressIndicator(text: 'Creating Post');
+              showSnackBar(context, 'Post Created');
+            });
+            //Backendless.data.withClass<Posts>().save(newPost);
           },
         ),
       ],
@@ -80,51 +140,41 @@ class _MainPageState extends State<MainPage> {
               );
             },
           ),
-
         ],
       ),
       body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.blue.shade700, Colors.lightBlue.shade50],
-            ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade700, Colors.lightBlue.shade50],
           ),
-          child: ListView(
-            children: [
-              Column(
-                children: [
-
-                  ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount:posts.length,
-                      itemBuilder: (context,index){
-                        return  PostCard(postTitle: posts[index].title, postText: posts[index].title, containsImage: posts[index].hasImage);
-                      }),
-
-                  //test post cards below !!
-                  /*  post_card(
-                    postTitle: 'hallo 1',
-                    postText:
-                        'this is some very long text to show that this text will fit into a very small screen without pixel overflowing...',
-                    containsImage: false,
-                  ),
-                  post_card(
-                    postTitle: 'hallo 2',
-                    postText: 'post text here',
-                    containsImage: false,
-                  ),
-                  post_card(
-                    postTitle: 'hallo 3',
-                    postText: 'post text here',
-                    containsImage: false,
-                  ), */
-                ],
-              ),
-            ],
-          )),
+        ),
+        child: Stack(
+          children: [
+            ListView(
+              children: [
+                Column(
+                  children: [
+                    ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: posts.length,
+                        itemBuilder: (context, index) {
+                          return PostCard(
+                            postTitle: posts[index].title,
+                            postText: posts[index].content,
+                            containsImage: posts[index].hasImage,
+                            dateTime: posts[index].created,
+                          );
+                        }),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
