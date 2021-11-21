c95c337f0e032f07308f90c80a0d8dea014f4297
Erdwurm05 <ruanfoord50@gmail.com>
2021-11-21 18:42:10 +0200

    post_card upgrades

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PostCard extends StatelessWidget {
   const PostCard(
@@ -20,13 +23,39 @@ class PostCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        tileColor: Colors.lightBlue,
-        title: Text("$postTitle"),
+        tileColor: Colors.lightBlue.shade400,
+        title: Text(
+          "$postTitle",
+          style: TextStyle(fontSize: 18),
+        ),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("$postText"),
-            Text("$dateTime"),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(7))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "$postText",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w600)),
+                    ),
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10, top: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("$dateTime"),
+                ],
+              ),
+            ),
           ],
         ),
         isThreeLine: true,
