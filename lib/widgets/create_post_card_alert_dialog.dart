2471e28855ed1b897d89ad3d52281de1c770b0ab
JancoSOUS <jancobuitendag@gmail.com>
2021-11-24 19:49:00 +0200

    Moved CreatePostCard to a new file

---
+++ lib/widgets/create_post_card_alert_dialog.dart
@@ -0,0 +1,79 @@
+import 'package:cut_info/services/helper_post.dart';
+import 'package:cut_info/widgets/snackbar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CreatePostCard extends StatelessWidget {
+  const CreatePostCard({
+    Key? key,
+    required this.postTitleController,
+    required this.postContentController,
+    required this.context,
+  }) : super(key: key);
+
+  final TextEditingController postTitleController;
+  final TextEditingController postContentController;
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    /////////////alertDialog//////////
+    AlertDialog alertDialog = AlertDialog(
+      backgroundColor: Colors.blue.shade300,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      title: Text('Create a new Post'),
+      content: Column(
+        children: [
+          TextField(
+            keyboardType: TextInputType.text,
+            controller: postTitleController,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), hintText: 'Enter Post Title.'),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                controller: postContentController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Post content.'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          child: Text(
+            'Cancel',
+            style: TextStyle(color: Colors.black),
+          ),
+          onPressed: () {
+            postTitleController.text = '';
+            postContentController.text = '';
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: Text(
+            'Save',
+            style: TextStyle(color: Colors.black),
+          ),
+          onPressed: () {
+            showSnackBar(context, "Creating Post");
+            Navigator.of(context).pop();
+            submitPost(postTitleController, postContentController);
+            showSnackBar(context, "Post created");
+          },
+        ),
+      ],
+    );
+    return (alertDialog);
+  }
+}
