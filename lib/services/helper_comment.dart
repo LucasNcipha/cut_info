ec2d984e57ee505b865589a624179cb1177f1624
JancoSOUS <jancobuitendag@gmail.com>
2021-12-05 21:14:12 +0200

    created submitComment() method to assist in Posting comments

--- lib/services/helper_comment.dart
+++ lib/services/helper_comment.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:backendless_sdk/backendless_sdk.dart';
 import 'package:cut_info/models/comment.dart';
+import 'package:cut_info/services/user_service.dart';
 
 Future<List<Comment>> recieveComments(String? objectID) async {
   sleep(Duration(microseconds: 200));
@@ -24,4 +25,20 @@ Future<List<Comment>> recieveComments(String? objectID) async {
   });
 
   return comments;
+}
+
+Future<void> submitComment(String commentContent, String postID) async {
+  String userName = getUsername();
+
+  Comment newComment =
+      new Comment(commentContent, DateTime.now(), userName, postID);
+
+  Map data = {
+    'comment': newComment.comment,
+    'created': newComment.created,
+    'user': newComment.userName,
+    'postID': newComment.postID
+  };
+
+  await Backendless.data.of("Comments").save(data);
 }
