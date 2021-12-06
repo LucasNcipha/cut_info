b48f9105e991048954bf528b2751374db439d5a8
LucasNcipha <42454023+LucasNcipha@users.noreply.github.com>
2021-12-06 17:00:17 +0200

    Update post.dart
    Saving course

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -4,13 +4,15 @@ class Posts {
   late bool hasImage;
   late DateTime created;
   late String objectId;
+  late String course;
 
   Posts(String title, String content, bool hasImage, DateTime created,
-      String objectId) {
+      String objectId, String course) {
     this.title = title;
     this.content = content;
     this.hasImage = hasImage;
     this.created = created;
     this.objectId = objectId;
+    this.course = course;
   }
 }
